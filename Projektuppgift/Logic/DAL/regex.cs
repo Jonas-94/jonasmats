8cf52efe3b65f6180809a65d21f6529551005b29
jonas-94 <jonas_2010@hotmail.com>
2020-11-16 10:57:26 +0100

    k

---
+++ Projektuppgift/Logic/DAL/regex.cs
@@ -0,0 +1,125 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Globalization;
+
+namespace Logic.DAL
+{
+    static class regex
+    {
+        static string ErrorMessage = "";
+
+        public static bool VerifyEmail(string email)
+        {
+            if (string.IsNullOrWhiteSpace(email))
+                return false;
+
+            try
+            {
+                // Normaliserr domänen
+                email = Regex.Replace(email, @"(@)(.+)$", DomainMapper,
+                                      RegexOptions.None, TimeSpan.FromMilliseconds(200));
+
+                // Undersöker domändelen av e-postmeddelandet och normaliserar det
+                string DomainMapper(Match match)
+                {
+                    // Använd IdnMapping-klassen för att konvertera Unicode-domännamn.
+                    var idn = new IdnMapping();
+
+                    // Dra ut och bearbeta domännamn(kastar ArgumentException på ogiltigt)
+                    string domainName = idn.GetAscii(match.Groups[2].Value);
+
+                    return match.Groups[1].Value + domainName;
+                }
+            }
+            catch (RegexMatchTimeoutException e)
+            {
+                return false;
+            }
+            catch (ArgumentException e)
+            {
+                return false;
+            }
+            try
+            {
+                return Regex.IsMatch(email,
+                    @"^[^@\s]+@[^@\s]+\.[^@\s]+$",
+                    RegexOptions.IgnoreCase, TimeSpan.FromMilliseconds(250));
+            }
+            catch (RegexMatchTimeoutException)
+            {
+                return false;
+            }
+        }
+        public static bool VerifyPassword(string password)
+        {
+
+            var input = password;
+            ErrorMessage = string.Empty;
+
+            if (string.IsNullOrWhiteSpace(input))
+            {
+                throw new Exception("Password should not be empty");
+            }
+
+            var hasNumber = new Regex(@"[0-9]+");
+            var hasUpperChar = new Regex(@"[A-Z]+");
+            //var hasMiniMaxChars = new Regex(@".{4-20}");
+            var hasLowerChar = new Regex(@"[a-z]+");
+            var hasSymbols = new Regex(@"[!@#$%^&*()_+=\[{\]};:<>|./?,-]");
+
+            if (!hasLowerChar.IsMatch(input))
+            {
+                ErrorMessage = "Password should contain At least one lower case letter";
+                return false;
+            }
+            else if (!hasUpperChar.IsMatch(input))
+            {
+                ErrorMessage = "Password should contain At least one upper case letter";
+                return false;
+            }
+            else if (!hasMiniMaxChars(input))
+            {
+                ErrorMessage = "Password should not be less than 4 or greater than 20 characters";
+                return false;
+            }
+            else if (!hasNumber.IsMatch(input))
+            {
+                ErrorMessage = "Password should contain At least one numeric value";
+                return false;
+            }
+
+            else if (!hasSymbols.IsMatch(input))
+            {
+                ErrorMessage = "Password should contain At least one special case characters";
+                return false;
+            }
+            else
+            {
+                return true;
+            }
+
+        }
+        public static bool checkForEmail(string email)
+        {
+            bool IsValid = false;
+            Regex r = new Regex(@"^([\w\.\-]+)@([\w\-]+)((\.(\w){2,3})+)$");
+            if (r.IsMatch(email))
+            {
+                IsValid = true;
+            }
+            return IsValid;
+        }
+
+        public static bool hasMiniMaxChars(string input)
+        {
+            if (input.Length >= 4 && input.Length <= 20)
+            {
+                return true;
+            }
+            return false;
+        }
+    }
+}
+
